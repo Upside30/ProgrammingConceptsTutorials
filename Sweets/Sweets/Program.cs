e7f433e85a03491c855192c0ba3df9356e36db1a
Marcin Wolf <marcinwolf363@gmail.com>
2017-09-26 15:07:39 +0100

    Sweets Project

---
+++ Sweets/Sweets/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int sweets = 40;
+            const int students = 14;
+            int studentsSweets;
+            int teachersSweets;
+
+            studentsSweets = sweets / students;
+            teachersSweets = sweets % students;
+            Console.WriteLine("Each student will get {0} sweets.", studentsSweets);
+            Console.WriteLine("The teacher will be left with {0} sweets.", teachersSweets);
+            //Console.ReadLine();
+        }
+    }
+}
